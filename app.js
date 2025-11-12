bb065632ef10847112f59c325a69b7ffc7684163
YourName <youremail@example.com>
2025-11-13 01:09:35 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,36 @@
+
+// const colors = ["red","Blue","Green","Purple","Orange","Pink","Yelow"]
+
+var BTN = document.querySelector(".Buttn")
+
+const value = function (){
+    buttn.className.toggleAttribute('dark')
+    return
+}
+
+BTN.toggleAttribute.className = 'Buttn'
+
+
+// value.style
+
+function ChalJa(){
+    document.querySelector('Buttn').toggleAttribute()
+}
+
+const hoga = document.querySelector('img')
+
+
+function increment(){
+    // increment.width + 5
+
+        // console.log("Kuch hoga")
+
+        hoga.width += 5
+}
+
+
+function decrement(){
+
+    hoga.width -= 5
+
+}
